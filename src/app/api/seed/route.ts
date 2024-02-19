7821c6c892831bddb52b7c43e2154539d1113159
ismaelarias <ismaeljarias@gmail.com>
2024-02-19 13:47:33 -0600

    add seed, fix prisma lib file types

---
+++ src/app/api/seed/route.ts
@@ -0,0 +1,21 @@
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function GET(request: Request) {
+
+  await prisma.todo.deleteMany(); // delete * from todo
+
+  await prisma.todo.createMany({
+    data: [
+      {description: 'Soul stone', complete: true},
+      {description: 'Power stone'},
+      {description: 'Time stone'},
+      {description: 'Space stone'},
+      {description: 'Reality stone'},
+    ]
+  })
+
+
+  return NextResponse.json({message: 'Seed Executed'});
+
+}
